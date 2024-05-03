d7bc9acbb0d2fa4f5c4fa62cd67b8c6ba4778b3f
kaMat Prahlad <kamatprahlad43@gmail.com>
2024-05-04 00:20:35 +0530

    git init

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./utils/Themes";
+
+const Container = styled.div`
+  background: ${({ theme }) => theme.bg};
+  width: 100%;
+  height: 100vh;
+`;
+
 function App() {
-  return <div className="App">
-    Gegg
-  </div>;
+  // hooks
+  const [darkMode, setDarkMode] = useState(false);
+
+  return (
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Container className="App">Podstream hbxbbxb</Container>
+    </ThemeProvider>
+  );
 }
 
 export default App;
