b881d8ddb4dd23da4ed92d260f6231d40f6a8572
kaMat Prahlad <kamatprahlad43@gmail.com>
2024-05-04 14:13:43 +0530

    git init

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import styled, { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "./utils/Themes";
 import Sidebar from "./components/Sidebar";
+import { BrowserRouter } from "react-router-dom";
 
 const Container = styled.div`
   display: flex;
@@ -18,10 +19,12 @@ function App() {
 
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-      <Container>
-        <Sidebar />
-        PodStream
-      </Container>
+      <BrowserRouter>
+        <Container>
+          <Sidebar />
+          PodStream
+        </Container>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,15 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 import { HomeRounded, CloseRounded } from "@mui/icons-material";
+import LOGOImage from "../Images/Logo.png";
+import { Link } from "react-router-dom";
 
 const MenuContainer = styled.div`
-  //   flex: 0.5;
-  //   flex-direction: column;
-  //   height: 100vh;
-  //   display: flex;
-  //   background-color: ${({ theme }) => theme.bg};
-  //   color: ${({ theme }) => theme.text_primary};
-
   flex: 0.5;
   flex-direction: column;
   height: 100vh;
@@ -18,21 +13,22 @@ const MenuContainer = styled.div`
   background-color: ${({ theme }) => theme.bg};
   color: ${({ theme }) => theme.text_primary};
 
-  //   @media (max-width: 1100px){
-  //     position:fixed;
-  //     z-index: 1000;
-  //     width: 100%;
-  //     max-width: 250px;
-  //     left: ${({ setMenuOpen }) => (setMenuOpen ? "0" : "")}
-  //     transition: 0.3s ease-in-out;
-  //   }
+    @media (max-width: 1100px){
+      position:fixed;
+      z-index: 1000;
+      width: 100%;
+      max-width: 250px;
+      left: ${({ setMenuOpen }) => (setMenuOpen ? "0" : "")}
+      transition: 0.3s ease-in-out;
+    }
 `;
 
 const Flex = styled.div`
+  justify-content: space-between;
   display: flex;
-  flex-direction: row;
   align-items: center;
-  justify-content: center;
+  padding: 0px 16px;
+  width: 86%;
 `;
 
 const Logo = styled.div`
@@ -40,10 +36,14 @@ const Logo = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  width: 100%;
   gap: 6px;
   font-weight: bold;
   font-size: 20px;
   margin: 16px 0px;
+`;
+const Image = styled.img`
+  height: 40px;
 `;
 
 const Close = styled.div`
@@ -52,23 +52,44 @@ const Close = styled.div`
     display: block;
   }
 `;
-const Elements = styled.div``;
+const Elements = styled.div`
+  padding: 4px 16px;
+  display: flex;
+  flex-direction: row;
+  box-sizing: border-box;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 12px;
+  cursor: pointer;
+  color: ${({ theme }) => theme.text_secondary};
+  width: 100%;
+  &:hover {
+    background-color: ${({ theme }) => theme.text_secondary + 50};
+  }
+`;
 const NavText = styled.div`
   padding: 12px 0px;
+  text-decoration: none;
 `;
 
 const Sidebar = () => {
   return (
     <MenuContainer>
-      <Flex></Flex>
-      <Logo>PodStream</Logo>
-      <Close>
-        <CloseRounded />
-      </Close>
-      <Elements>
-        <HomeRounded />
-        <NavText>Dashboard</NavText>
-      </Elements>
+      <Flex>
+        <Logo>
+          <Image src={LOGOImage} />
+          PodStream
+        </Logo>
+        <Close>
+          <CloseRounded />
+        </Close>
+      </Flex>
+      <Link to="/">
+        <Elements>
+          <HomeRounded />
+          <NavText>Dashboard</NavText>
+        </Elements>
+      </Link>
     </MenuContainer>
   );
 };
