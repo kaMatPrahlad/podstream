16aed98da919a7b6f817ac01baee5d9e027b6987
KaMat Prahlad <kamatprahlad43@gmail.com>
2024-05-07 00:08:30 +0530

    git init

--- client/src/pages/Favourites.jsx
+++ client/src/pages/Favourites.jsx
@@ -1,9 +1,33 @@
 import React from "react";
 import PodcastCard from "../components/PodcastCard";
+import styled from "styled-components";
 
-const Container = styled.div``;
-const Topic = styled.div``;
-const FavouriteContainer = styled.div``;
+const Container = styled.div`
+  padding: 20px 30px;
+  padding-bottom: 200px;
+  height: 100%;
+  overflow-y: scroll;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+const Topic = styled.div`
+  color: ${({ theme }) => theme.text_primary};
+  font-size: 24px;
+  font-weight: 540;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const FavouriteContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 14px;
+  padding: 18px 6px;
+  @media (max-width: 550px) {
+    justify-content: center;
+  }
+`;
 
 function Favourites() {
   return (
